06723a0ca6f6bdb41de863260b069ca7c1979a95
Shreyansh Singh <singh.shreyansh1756@gmail.com>
2026-04-01 18:35:59 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const morgan = require("morgan");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Finance Dashboard Backend API is running" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}
+    http://localhost:5000/`);
+});
