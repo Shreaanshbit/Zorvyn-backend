6fc44bf2c3e07db291ec92f5a3576ea578b3cd5d
Shreyansh Singh <singh.shreyansh1756@gmail.com>
2026-04-02 22:06:02 +0530

    change in viewer role: can add their entries for dashboard

--- src/routes/recordRoutes.js
+++ src/routes/recordRoutes.js
@@ -10,7 +10,7 @@ const allowRoles = require("../middlewares/roleMiddleware");
 
 const router = express.Router();
 
-router.post("/", protect, allowRoles("admin"), createRecord);
+router.post("/", protect, allowRoles("admin","viewer"), createRecord);
 router.get("/", protect, allowRoles("admin", "analyst", "viewer"), getRecords);
 router.patch("/:id", protect, allowRoles("admin"), updateRecord);
 router.delete("/:id", protect, allowRoles("admin"), deleteRecord);
